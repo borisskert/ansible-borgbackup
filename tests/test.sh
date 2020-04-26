2086486357bbf321489508aa18cfcb18c057b63a
Boris Skert <boris.skert@gmail.com>
2020-04-26 13:15:58 +0200

    Create initial project

---
+++ tests/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+vagrant up --provision
+
+export ANSIBLE_CONFIG=ansible.cfg
+
+echo "Waiting for answer on port 22..."
+while ! timeout 1 nc -z 192.168.33.82 22; do
+  sleep 0.2
+done
+
+ansible-playbook -i inventory.ini test.yml $*
+
+ansible-playbook -i inventory.ini test.yml \
+  | grep -q 'changed=0.*failed=0' \
+  && (echo 'Idempotence test: pass' && exit 0) \
+  || (echo 'Idempotence test: fail' && exit 1)
+
+#vagrant destroy -f
